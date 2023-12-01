00f772ed8b6c53edabaa792fcd5dd5c3a3784dcd
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-01 16:56:20 +0100

    Update App.jsx

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -7,12 +7,19 @@ function App() {
       <div id="game-container">
       <ol id="players">
         <li> 
+          <span className="player">
           <span className="player-name">Player 1</span>
           <span className="player-symbol">X</span>
+          </span>
           </li>
+         <button>Edit</button>
         <li>
+        <span className="player">
         <span className="player-name">Player 2</span>
           <span className="player-symbol">O</span>
+        
+        </span>
+        <button>Edit</button>
         </li>
       </ol>
 
